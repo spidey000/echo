055de705a2b0146b1a7bf32bec5c552a327a5e64
gg <spidey00@gmail.com>
2026-02-09 14:41:38 +0100

    chore: stop tracking generated artifacts and APK binaries

--- SaidIt/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_eu_mrogalski_saidit_EchoApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "eu.mrogalski.saidit.EchoApp"
-)
-public final class _eu_mrogalski_saidit_EchoApp {
-}
