bab68a1dd3d2edb2cf3661124ed67dbbbe4d1c7d
gg <spidey00@gmail.com>
2026-03-09 19:33:57 +0100

    chore: bump version to 2.0.4-debug.2

--- SaidIt/build.gradle.kts
+++ SaidIt/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.spidey000.echofork"
         minSdk = 30
         targetSdk = 34
-        versionCode = 20
-        versionName = "2.0.4-debug.1"
+        versionCode = 21
+        versionName = "2.0.4-debug.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
